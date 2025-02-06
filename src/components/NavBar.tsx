7aee89786064c192b7d3756f033742a43fc735da
pocho18 <joaknuttzen06@gmail.com>
2025-02-05 21:45:25 -0300

    Add Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import styles from '../modules/NavBar.module.css'
+import { DocumentTextIcon, UserIcon } from '@heroicons/react/16/solid'
 
 export default function NavBar() {
   return (
@@ -6,7 +7,28 @@ export default function NavBar() {
         <div>
             <h1>JoaK - WebDev</h1>
         </div>
-        <div></div>
+        <div>
+          <ul>
+            <li>
+              <a href="/">
+                <img src="./src/assets/github-mark-white.svg" alt="Github logo" />
+                <p>Github</p>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <DocumentTextIcon width={35} height={35} />
+                <p>Currículum</p>
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <UserIcon width={35} height={35} />
+                <p>Contacto</p>
+              </a>
+            </li>
+          </ul>
+        </div>
     </nav>
   )
 }
